3c9443aadf8f4fd14ecf5afbf0c4e23888e4202e
SAmy121212 <samybenmaajouz5@gmail.com>
2025-03-27 17:42:26 +0100

    MAJ splashmem 😎

--- Zonz_project/Projet_splashmem/splashmem-main/src/main.c
+++ Zonz_project/Projet_splashmem/splashmem-main/src/main.c
@@ -14,14 +14,9 @@
 int          quitting      = 0;
 SDL_Window*  window        = NULL;
 SDL_Surface* screenSurface = NULL;
-static uint8_t lancer_jeu = 0;
-static int32_t value_pl = 0;
-static int32_t value_cr = 0;
 
 #define WINDOW_WIDTH 650
 #define WINDOW_HEIGHT 400
-
-void creat_page_acceuil(void) ;
 
 /* ------------------------------------------------------------------------- */
 /*                                                                           */
@@ -41,33 +36,28 @@ int SDLCALL watch(void* userdata, SDL_Event* event)
 /* ------------------------------------------------------------------------- */
 int main(int argc, char* argv[])
 {
-    creat_page_acceuil();
-
-    if(lancer_jeu == 1)
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
     {
-        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
-        {
-            SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
-        }
-
-        window = SDL_CreateWindow("SplashMem", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIN_SIZE,
-                                WIN_SIZE, SDL_WINDOW_SHOWN);
-        SDL_AddEventWatch(watch, NULL);
-
-        if (argc != 5)
-        {
-            printf("Wrong argument number\n");
-        }
-        inits(argc, argv);
-
-        main_loop();
-
-        SDL_DelEventWatch(watch, NULL);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-
-        exit(0);
+        SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
     }
+
+    window = SDL_CreateWindow("SplashMem", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIN_SIZE,
+                            WIN_SIZE, SDL_WINDOW_SHOWN);
+    SDL_AddEventWatch(watch, NULL);
+
+    if (argc != 5)
+    {
+        printf("Wrong argument number\n");
+    }
+    inits(argc, argv);
+
+    main_loop();
+
+    SDL_DelEventWatch(watch, NULL);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    exit(0);
 } // main
 
 void renderText(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x, int y, SDL_Color color) {
@@ -94,14 +84,4 @@ void renderText(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x,
     SDL_Rect dstRect = {x, y, surface->w, surface->h};
     SDL_RenderCopy(renderer, texture, NULL, &dstRect);
     SDL_DestroyTexture(texture);  // Libérer la texture après l'affichage
-}
-
-int32_t get_number_player(void)
-{
-    return value_pl;
-}
-
-int32_t get_number_credit(void)
-{
-    return value_cr;
 }
--- Zonz_project/Projet_splashmem/splashmem-main/src/main.h
+++ Zonz_project/Projet_splashmem/splashmem-main/src/main.h
@@ -1,5 +1,2 @@
 #include <stdint.h>
 
-int32_t get_number_player(void);
-
-int32_t get_number_credit(void);
